4cb9ddc53d67794f30f6f499e77c9ecc9d49a68a
Ebrahim <ebrahimgreat@gmail.com>
2026-02-14 20:49:44 +0500

    Fix SSL deprecation warning

--- config/runtime.exs
+++ config/runtime.exs
@@ -49,9 +49,8 @@ if config_env() == :prod do
   maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
 
   config :crohnjobs, Crohnjobs.Repo,
-    ssl: true,
-    ssl_opts: [verify: :verify_none],
     url: database_url,
+    ssl: [verify: :verify_none],
     pool_size: String.to_integer(System.get_env("POOL_SIZE") || "2"),
     socket_options: maybe_ipv6
 
