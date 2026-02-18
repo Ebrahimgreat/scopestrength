713f65aba539158aebed6afe6da0daacec881a43
Ebrahim <ebrahimgreat@gmail.com>
2026-02-18 13:14:55 +0500

    Leads added to capture users, also Admin account has been added

---
+++ lib/crohnjobs/leads.ex
@@ -0,0 +1,28 @@
+defmodule Crohnjobs.Leads do
+  import Ecto.Query
+  alias Crohnjobs.Repo
+  alias Crohnjobs.Leads.Lead
+
+  @doc """
+  Checks if an email has already been used for a demo.
+  Returns {:ok, lead} if found, {:error, :not_found} if new email.
+  """
+  def check_lead(email) do
+    normalized_email = String.downcase(email)
+
+    case Repo.get_by(Lead, email: normalized_email) do
+      nil -> {:error, :not_found}
+      lead -> {:ok, lead}
+    end
+  end
+
+  def create_lead(attrs) do
+    %Lead{}
+    |> Lead.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def list_leads do
+    Repo.all(from l in Lead, order_by: [desc: l.inserted_at])
+  end
+end
--- lib/crohnjobs_web/controllers/demo_controller.ex
+++ lib/crohnjobs_web/controllers/demo_controller.ex
@@ -2,19 +2,38 @@ defmodule CrohnjobsWeb.DemoController do
   use CrohnjobsWeb, :controller
 
   alias Crohnjobs.Account
+  alias Crohnjobs.Leads
   alias CrohnjobsWeb.UserAuth
 
-  def create(conn, _params) do
-    case Account.generate_demo_account() do
-      {:ok, user} ->
+  def create(conn, %{"email" => email}) do
+    case Leads.check_lead(email) do
+      {:ok, _existing_lead} ->
+        # Email already used for demo - show friendly message
         conn
-        |> put_flash(:info, "Welcome to the demo! Your email: #{user.email} | Password: Demodemo1234")
-        |> UserAuth.log_in_user(user)
-
-      {:error, _reason} ->
-        conn
-        |> put_flash(:error, "Failed to create demo account. Please try again.")
+        |> put_flash(:info, "You've already tried our demo! If you like the service please contact ebrahim@scopestrength.com")
         |> redirect(to: ~p"/users/log_in")
+
+      {:error, :not_found} ->
+        # New email - create demo account
+        case Account.generate_demo_account() do
+          {:ok, user} ->
+            Leads.create_lead(%{email: email, demo_user_id: user.id})
+
+            conn
+            |> put_flash(:info, "Welcome to the demo! Your email: #{user.email} | Password: Demodemo1234")
+            |> UserAuth.log_in_user(user)
+
+          {:error, _reason} ->
+            conn
+            |> put_flash(:error, "Failed to create demo account. Please try again.")
+            |> redirect(to: ~p"/users/log_in")
+        end
     end
+  end
+
+  def create(conn, _params) do
+    conn
+    |> put_flash(:error, "Please enter your email to try the demo.")
+    |> redirect(to: ~p"/users/log_in")
   end
 end
