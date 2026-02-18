b265a322adf24f79ba93ff7753218a74250fb602
Ebrahim <ebrahimgreat@gmail.com>
2026-02-18 09:58:38 +0500

    demo passwords updated

---
+++ priv/repo/migrations/20260218045738_update_demo_passwords.exs
@@ -0,0 +1,19 @@
+defmodule Crohnjobs.Repo.Migrations.UpdateDemoPasswords do
+  use Ecto.Migration
+
+  def up do
+    # Update all demo accounts to use the standard demo password "Demodemo1234"
+    hashed = Bcrypt.hash_pwd_salt("Demodemo1234")
+
+    execute """
+    UPDATE users
+    SET hashed_password = '#{hashed}'
+    WHERE email LIKE 'demo_%@scopestrength.com'
+    """
+  end
+
+  def down do
+    # Cannot reverse password changes
+    :ok
+  end
+end
